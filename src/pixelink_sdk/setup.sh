a65f6a1755e2f676959de8351fc4a52342eb9b6f
qianqian121 <a@b.com>
2018-03-29 22:37:55 -0700

    fix pixelink cmake

--- src/pixelink_sdk/setup.sh
+++ src/pixelink_sdk/setup.sh
@@ -2,9 +2,9 @@
 
 #sudo apt-get update && sudo apt-get install -y libsdl2-2.0 glade ffmpeg
 
-export PIXELINK_SDK_INC=/home/dev/project/pixelink_sdk/include
+export PIXELINK_SDK_INC=/home/dev/project/github/GIFT-Grab/src/pixelink_sdk/include
 
-export PIXELINK_SDK_LIB=/home/dev/project/pixelink_sdk/lib
+export PIXELINK_SDK_LIB=/home/dev/project/github/GIFT-Grab/src/pixelink_sdk/lib
 
 export LD_LIBRARY_PATH=$PIXELINK_SDK_LIB:$LD_LIBRARY_PATH
 
