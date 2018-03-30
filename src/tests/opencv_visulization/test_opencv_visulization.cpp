42fe13bb58c034c1c6f2483a04d90f61be5e7e43
qianqian121 <a@b.com>
2018-03-29 20:52:20 -0700

    opencv source target initial commit
    wrong code. Do not use.

--- src/tests/opencv_visulization/test_opencv_visulization.cpp
+++ src/tests/opencv_visulization/test_opencv_visulization.cpp
@@ -1,7 +1,25 @@
 //
 // Created by dev on 3/28/18.
 //
+#include "videoframe.h"
+#include "videosourcefactory.h"
+#include "videotargetfactory.h"
 
 int main(int argc, char* argv[]) {
+    gg::ColourSpace colour = gg::BGRA;
+    std::string filepath = "data/video_15frames_30fps.avi";
+    gg::VideoSourceFactory & factory = gg::VideoSourceFactory::get_instance();
+
+    IVideoSource * source = nullptr;
+
+    source = factory.create_file_reader(filepath, colour);
+
+    gg::VideoTargetFactory & factory_target = gg::VideoTargetFactory::get_instance();
+    gg::IVideoTarget * target = nullptr;
+    gg::Codec codec = gg::Codec::Xvid;
+    target = factory_target.create_file_writer(codec, "opencv_output.avi", 30.0);
+
+    source->attach(target);
+    delete target;
     return 0;
 }
